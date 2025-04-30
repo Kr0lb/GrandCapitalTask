d757e7162800631696457dd8b7db023499efe1e1
Kr0lb <pivamaks@gmail.com>
2025-04-30 15:56:01 +0300

    change test containers

--- src/test/java/by/grc/GrandCapitalTask/GrandCapitalTestContainer.java
+++ src/test/java/by/grc/GrandCapitalTask/GrandCapitalTestContainer.java
@@ -25,7 +25,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @Testcontainers
 @SpringBootTest
 @AutoConfigureMockMvc
-public class GrandCapitalTestContainers {
+public class GrandCapitalTestContainer {
 
     @Value("${security.jwt.secret-key}")
     private String secretKey;
